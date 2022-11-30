841cea4513dbdea8cb7de9fec2062dd7b57c9ba0
aisuluu <is6116@gmail.com>
2022-11-30 11:55:45 +0600

    admin, search, filter

--- main/models.py
+++ main/models.py
@@ -9,3 +9,5 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'{self.author.username} -> {self.body}'
